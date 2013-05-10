edf6ca51d26ab85be2efd69ecc9d9b643a135e07
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-09 19:56:03 -0500

    First solution Student Schema active record

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,38 @@
 require_relative '../../db/config'
+require 'date'
+
+
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+  validate :email_format 
+  validates :email, :uniqueness => true
+  validate :age_restrict
+  validates :phone, format: { with: /.{9,}/ }
+  
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    today = Date.today
+    birthday_past?(today) ? today.year - birthday.year : today.year - birthday.year - 1
+  end
+
+  def birthday_past?(today)
+
+    today.month > birthday.month && today.day > birthday.day    
+  end
+
+  def email_format
+    check = email =~ /.+@.+\..{2,}/ 
+    
+    errors.add(:email, "Wrong email format") if check != 0
+  end
+
+  def age_restrict
+    errors.add(:age_restrict, "Not eligible to be a student, too young") if age < 5
+  end
+
+end
